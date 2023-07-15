88d4743ed5a3358fc328d6ed171adf10da1c7a99
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-15 10:37:55 +0000

    Installed coverage and added last tests to cover 100% of my code

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mristimaki-hellodjango-111diegli2e.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['8000-mristimaki-hellodjango-mpz2e7hj92g.ws-eu101.gitpod.io']
 
 
 # Application definition
