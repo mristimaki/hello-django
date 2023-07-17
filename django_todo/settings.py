d338159c7ad999ad660552e50e78850e87190db9
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-17 19:19:23 +0000

    removed line from procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mristimaki-hellodjango-nc05wzsp0iy.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['8000-mristimaki-hellodjango-22iucwg42ak.ws-eu101.gitpod.io']
 
 
 # Application definition
